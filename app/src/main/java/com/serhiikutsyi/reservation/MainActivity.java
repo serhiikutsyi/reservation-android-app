48e80e876f0fb2fdedacedfbc252720495d5fea5
Sergii Kutsyi <s.kutsyi@spd-ukraine.com>
2017-09-11 01:09:27 +0300

    Add ActionBar

--- app/src/main/java/com/serhiikutsyi/reservation/MainActivity.java
+++ app/src/main/java/com/serhiikutsyi/reservation/MainActivity.java
@@ -1,9 +1,14 @@
 package com.serhiikutsyi.reservation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,7 +25,6 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mRetrieveButton;
     private RecyclerView recyclerView;
     private List<Reservation> reservations;
 
@@ -38,25 +42,47 @@ public class MainActivity extends AppCompatActivity {
         ReservationAdapter adapter = new ReservationAdapter(reservations);
         recyclerView.setAdapter(adapter);
 
-        mRetrieveButton = (Button)
-                findViewById(R.id.retrieve_button);
-        mRetrieveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                App.getApi().getReservations().enqueue(new Callback<List<Reservation>>() {
-                    @Override
-                    public void onResponse(Call<List<Reservation>> call, Response<List<Reservation>> response) {
-                        reservations.clear();
-                        reservations.addAll(response.body());
-                        recyclerView.getAdapter().notifyDataSetChanged();
-                    }
+        getReservations();
 
-                    @Override
-                    public void onFailure(Call<List<Reservation>> call, Throwable t) {
-                        Toast.makeText(MainActivity.this, "An error occurred during networking", Toast.LENGTH_SHORT).show();
-                    }
-                });
+        Toolbar mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolBar);
+    }
+
+    public void getReservations() {
+        App.getApi().getReservations().enqueue(new Callback<List<Reservation>>() {
+            @Override
+            public void onResponse(Call<List<Reservation>> call, Response<List<Reservation>> response) {
+                reservations.clear();
+                reservations.addAll(response.body());
+                recyclerView.getAdapter().notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Reservation>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "An error occurred during networking", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mMenuInflater = getMenuInflater();
+        mMenuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        if (item.getItemId() == R.id.action_add) {
+            Toast.makeText(MainActivity.this,
+                    "You have clicked on Add action TBD",
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+        if (item.getItemId() == R.id.action_refresh) {
+            getReservations();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
